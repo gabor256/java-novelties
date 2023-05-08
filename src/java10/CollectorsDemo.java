fc805594be60596cbbc0224e07332fc7bced02c1
Roszjár Gábor <groszjar@porschehungaria.hu>
2023-05-08 16:44:00 +0200

    java 10 - upd

--- src/java10/CollectorsDemo.java
+++ src/java10/CollectorsDemo.java
@@ -7,7 +7,12 @@ public class CollectorsDemo {
     public static void main(String[] args) {
         List<Integer> list = List.of(15, 20, 17, 30);
         List<Integer> newList = list.stream().filter(i -> i % 3 == 0).collect(Collectors.toUnmodifiableList()); //Java 10
-        newList.add(40); // És itt nincs hibajelzés de szétszáll az egész
+        System.out.println(newList);
+        // newList.add(40); immutable, elszáll (Idea nem jelez hibát)
+
+        List<Integer> copyOfList = List.copyOf(list);
+        System.out.println(copyOfList);
+        // copyOfList.add(4); - a második lista is immutable, itt is elszállna
 
     }
 }
